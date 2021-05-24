c91140d227ed8e34da339eba61636e0cd11f9b52
richlaw1986 <54599156+richlaw1986@users.noreply.github.com>
2021-05-24 18:04:31 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 
-
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -42,8 +41,12 @@ def load_data():
 data_two = load_data()
 
 st.sidebar.markdown("### Choose View")
-select = st.sidebar.selectbox('Metric', ['Absolute Trends', 'Relative Trend'], key='5')
+select = st.sidebar.selectbox('Metric', ['Relative Trend', 'Absolute Trends'], key='5')
 if not st.sidebar.checkbox("Hide", False, key=5):
+        if select == 'Relative Trend':
+            st.markdown("#### The graph shows estimated weekly search volume by brand - by combining a relative Google Trends score over the past 5 years with an estimated search volume for each brand. Zoom into to any time period over the past 5 years by selecting a portion of the graph, then zoom back out by double-clicking.")
+            fig_31 = px.line(data_two, x="Date", y="Estimated Weekly Search Volume", color="Brand",  title='Relative Trends by Brand')
+            st.plotly_chart(fig_31, use_container_width=True)
     if select == 'Absolute Trends':
         st.markdown("#### The graphs show the trend for each brand in isolation on a weekly basis over the past 5 years.  Zoom into to any time period over the past 5 years by selecting a portion of the graph, then zoom back out by double-clicking.")
         fig_26 = px.line(data, x="Date", y="Boohoo",  title='Boohoo Absolute Trend')
@@ -56,7 +59,3 @@ if not st.sidebar.checkbox("Hide", False, key=5):
         st.plotly_chart(fig_29, use_container_width=True)
         fig_30 = px.line(data, x="Date", y="Nasty Gal",  title="Nast Galy Absolute Trend")
         st.plotly_chart(fig_30, use_container_width=True)
-    if select == 'Relative Trend':
-        st.markdown("#### The graph shows estimated weekly search volume by brand - by combining a relative Google Trends score over the past 5 years with an estimated search volume for each brand. Zoom into to any time period over the past 5 years by selecting a portion of the graph, then zoom back out by double-clicking.")
-        fig_31 = px.line(data_two, x="Date", y="Estimated Weekly Search Volume", color="Brand",  title='Relative Trends by Brand')
-        st.plotly_chart(fig_31, use_container_width=True)
